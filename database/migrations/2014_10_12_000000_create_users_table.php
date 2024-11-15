d245fd1de4ea5c0c1dfee6e9b106926ac030b396
K B M Zahid Hasan <zahid6630gitlab@gmail.com>
2024-11-15 20:41:51 +0600

    Migration files created and remove public from the root url

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id')->unsigned();
+            $table->string('name', 100);
+            $table->enum('account_type', [1,2])->comment('1=Individual, 2=Business');
+            $table->double('balance')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ database/migrations/2024_11_15_143227_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->enum('transaction_type', [1,2])->comment('1=Deposit, 2=Withdrawal');
+            $table->decimal('amount', 10, 2)->default(0);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
